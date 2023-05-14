2e9e80e0d8df94d9fac3a904f188d0c318b82254
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-05-14 18:11:57 +0000

    Fix traling whitespace error

--- blogstar/urls.py
+++ blogstar/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summer_note/', include('django_summernote.urls'))
+    path('summer_note/', include('django_summernote.urls')),
 ]
