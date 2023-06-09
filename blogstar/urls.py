f00264f56887a64fb2fcad0baff977935ed237e5
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-06-09 15:54:57 +0000

    Add logo image

--- blogstar/urls.py
+++ blogstar/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from blog import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
 ]
