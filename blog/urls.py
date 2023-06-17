f38b66deb6f7366b401ede30cec5c08a7142d0b5
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-06-17 12:50:14 +0000

    Fix URL conflict between post detail and category views
    Updated the URL pattern for post detail view to use <slug:post_slug> to avoid conflict with the category views (sports, music, art). Now both the post detail and category views can coexist without issues.

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('sports/', views.sports_view, name='sports'),
     path('music/', views.music_view, name='music'),
     path('art/', views.art_view, name='art'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
+    path('<slug:post_slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', views.PostList.as_view(), name='home')
 ]
