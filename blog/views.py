d24a5fc7c2d91e33cb4c65ec835a57abdc318afd
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-06-17 13:01:27 +0000

    Fix URL routing and view handling for post detail page

--- blog/views.py
+++ blog/views.py
@@ -13,9 +13,9 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-    def get(self, request, slug, *args, **kwargs):
+    def get(self, request, post_slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(queryset, slug=post_slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
