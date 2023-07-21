10f926bfefb140997113118fb88a5fbf76d9b335
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-07-21 18:32:31 +0000

    Turn debug to false

--- blogstar/settings.py
+++ blogstar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['blog-star.herokuapp.com',
                  '8000-gabriel5638-blogwebsite-vrndp4etmrz.ws-eu102.gitpod.io'
