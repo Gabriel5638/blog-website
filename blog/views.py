7d070b118a57db56f4f3dac8ca687660f18f7bdb
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-07-21 14:12:44 +0000

    Add missing docstrings to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,19 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import DeleteView
-from django.urls import reverse_lazy
-from .models import Post
-from .forms import CommentForm
-from .forms import CreationForm
 from django.contrib import messages
-from django.shortcuts import redirect
 from django.db.models import Count
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from .models import Post
+from django.http import JsonResponse
+from .forms import CommentForm, CreationForm
 
 
 class PostList(generic.ListView):
     """
-    Display a list of published posts ordered by the number of likes and creation date.
+    Display a list of published posts ordered by the number of likes and date.
     """
     model = Post
     queryset = Post.objects.filter(status=1).annotate(
@@ -25,6 +23,9 @@ class PostList(generic.ListView):
 
 
 class UserPosts(LoginRequiredMixin, generic.ListView):
+    """
+    Display a list of posts created by the current user.
+    """
     model = Post
     template_name = 'user_posts.html'
     context_object_name = 'user_posts'
@@ -37,7 +38,30 @@ class UserPosts(LoginRequiredMixin, generic.ListView):
         return queryset
 
 
+class PostLike(View):
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+            liked = False
+        else:
+            post.likes.add(request.user)
+            liked = True
+
+        # Add success message based on the like action
+        if liked:
+            messages.success(request, 'Post liked successfully!')
+        else:
+            messages.success(request, 'Post unliked successfully!')
+
+        # Redirect back to the post detail page
+        return redirect('post_detail', post_slug=slug)
+
+
 class TrendingPosts(generic.ListView):
+    """
+    Display a list of posts ordered by the number of likes received.
+    """
     model = Post
     template_name = "trending_posts.html"
     context_object_name = "trending_posts"
@@ -47,47 +71,20 @@ class TrendingPosts(generic.ListView):
         # Retrieve the posts ordered by the number of likes they have received
         queryset = Post.objects.annotate(num_likes=Count('likes')).filter(
             num_likes__gt=0).order_by('-num_likes')
-
         return queryset
 
 
 class PostDetail(View):
+    """
+    Display a post and its details, along with comments and like status.
+    Allow users to post comments on the current post.
+    """
+
     def get(self, request, post_slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=post_slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        # Get the absolute URL of the current post
-        post_url = request.build_absolute_uri(
-            reverse('post_detail', args=[post.slug]))
-
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": False,
-                "liked": liked,
-                "comment_form": CommentForm(),
-                "post_url": post_url
-            },
-        )
-
-
-class PostDetail(View):
-    def get(self, request, post_slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=post_slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=request.user.id).exists():
-            liked = True
-
-        # Get the absolute URL of the current post
+        liked = post.likes.filter(id=request.user.id).exists()
         post_url = request.build_absolute_uri(reverse('post_detail', args=[post.slug]))
 
         return render(
@@ -107,10 +104,7 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=post_slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=request.user.id).exists():
-            liked = True
-
+        liked = post.likes.filter(id=request.user.id).exists()
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
@@ -118,11 +112,8 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
-
-            # Add a message to indicate the comment is awaiting approval
             messages.info(request, "Your comment has been submitted and is awaiting approval.")
 
-        # Get the absolute URL of the current post
         post_url = request.build_absolute_uri(reverse('post_detail', args=[post.slug]))
 
         return render(
@@ -137,8 +128,8 @@ class PostDetail(View):
                 "post_url": post_url
             },
         )
-    
-      
+
+
 def edit_post(request, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
 
@@ -158,39 +149,21 @@ def edit_post(request, post_slug):
 
     return render(request, 'edit_post.html', context)
 
-    def post(self, request, post_slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=post_slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
 
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
+class DeletePost(DeleteView):
+    """
+    Delete a post.
+    """
+    model = Post
+    template_name = 'user_posts.html'
+    success_message = 'Post has been successfully deleted.'
 
-        # Get the absolute URL of the current post
-        post_url = request.build_absolute_uri(reverse('post_detail', args=[post.slug]))
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER') or reverse('home')
 
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "comment_form": comment_form,
-                "liked": liked,
-                "post_url": post_url
-            },
-        )
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, self.success_message)
+        return super().delete(request, *args, **kwargs)
 
 
 def create_posts(request):
@@ -202,11 +175,7 @@ def create_posts(request):
             post.status = 1  # Set the default status to represent published
             post.save()
             messages.success(request, 'You have successfully posted an item!')
-
-            # Determine the category of the posted item
             category = post.category
-
-            # Redirect to the respective category
             if category == 'sports':
                 return redirect('sports')
             elif category == 'music':
@@ -215,7 +184,6 @@ def create_posts(request):
                 return redirect('art')
             elif category == 'gaming':
                 return redirect('gaming')
-
     else:
         postitem_form = CreationForm()
 
@@ -226,37 +194,13 @@ def create_posts(request):
     return render(request, "create_posts.html", context)
 
 
-class DeletePost(DeleteView):
-    model = Post
-    template_name = 'user_posts.html' 
-    success_message = 'Post has been successfully deleted.'
-
-    def get_success_url(self):
-        return self.request.META.get('HTTP_REFERER') or reverse('home')
-
-    def delete(self, request, *args, **kwargs):
-        messages.success(request, self.success_message)
-        return super().delete(request, *args, **kwargs)
-
-
-class PostLike(View):
-    def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Post, slug=slug)
-        if post.likes.filter(id=request.user.id).exists():
-            post.likes.remove(request.user)
-        else:
-            post.likes.add(request.user)
-
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
-
 def sports_view(request):
     sports_posts = Post.objects.filter(
         category='sports',
         status=1
     ).order_by('-created_on')
 
-    paginator = Paginator(sports_posts, 6)  # Display 6 posts per page
+    paginator = Paginator(sports_posts, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -269,7 +213,7 @@ def music_view(request):
         status=1
     ).order_by('-created_on')
 
-    paginator = Paginator(music_posts, 6)  # Display 6 posts per page
+    paginator = Paginator(music_posts, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -282,7 +226,7 @@ def art_view(request):
         status=1
     ).order_by('-created_on')
 
-    paginator = Paginator(art_posts, 6)  # Display 6 posts per page
+    paginator = Paginator(art_posts, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -295,7 +239,7 @@ def gaming_view(request):
         status=1
     ).order_by('-created_on')
 
-    paginator = Paginator(gaming_posts, 6)  # Display 6 posts per page
+    paginator = Paginator(gaming_posts, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
